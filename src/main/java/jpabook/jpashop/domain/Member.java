4b300ff619b4049be6711ca28f94b0bcb72abe44
smpark1020 <smpark911020@gmail.com>
2021-04-29 13:00:51 +0900

    엔티티 클래스 개발1

--- src/main/java/jpabook/jpashop/domain/Member.java
+++ src/main/java/jpabook/jpashop/domain/Member.java
@@ -1,7 +1,27 @@
 package jpabook.jpashop.domain;
 
-import javax.persistence.Entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
+@Getter @Setter
 public class Member {
+
+    @Id @GeneratedValue
+    @Column(name = "member_id")
+    private Long id;
+
+    private String name;
+
+    @Embedded
+    private Address address;
+
+    @OneToMany(mappedBy = "member")
+    private List<Order> orders = new ArrayList<>();
+
 }
